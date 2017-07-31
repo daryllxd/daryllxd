3522f69f85ca31849b4182a2c7de4a77326db212
Daryll Santos <daryll.santos@gmail.com>
2017-07-31 20:56:49 +0800

    Add DateRange value object.

---
+++ app/models/date_range.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+class DateRange
+  attr_reader :start_date, :end_date
+
+  def initialize(start_date:, end_date:)
+    @start_date = start_date
+    @end_date = end_date
+  end
+end
---
+++ spec/models/date_range_spec.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+require 'timecop'
+
+RSpec.describe DateRange, type: :value do
+  it_should_behave_like('value_object_with_setters', 'start_date', 'end_date')
+end
