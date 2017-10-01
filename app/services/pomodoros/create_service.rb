af8a56989e0ab0c41a2558c73d04b0f7f2579baa
Daryll Santos <daryll.santos@gmail.com>
2017-10-01 20:22:59 +0800

    A tag must be present when logging a Pomodoro.

--- app/services/pomodoros/create_service.rb
+++ app/services/pomodoros/create_service.rb
@@ -3,13 +3,15 @@ module Pomodoros
   class CreateService
     attr_reader :description, :duration, :tags
 
-    def initialize(description:, duration:, tags: [])
+    def initialize(description:, duration:, tags:)
       @description = description
       @duration = duration
       @tags = tags
     end
 
     def call
+      return DaryllxdError.new(I18n.t('pomodoro.create.error_no_tags')) unless tags.present?
+
       ActiveRecord::Base.transaction do
         create_pomodoro
         create_tags
--- spec/services/pomodoros/create_service_spec.rb
+++ spec/services/pomodoros/create_service_spec.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
-RSpec.describe Pomodoros::CreateService do
+RSpec.describe Pomodoros::CreateService, type: :service do
   context 'happy path' do
     let!(:programming_activity_tag) { create(:activity_tag, :programming) }
     let!(:daryllxd_activity_tag) { create(:activity_tag, :daryllxd) }
 
     let!(:created_pomodoro) do
-      described_class.new(
+      execute.call(
         duration: 25,
         description: 'Did things',
         tags: [programming_activity_tag, daryllxd_activity_tag]
-      ).call
+      )
     end
 
     it 'creates a pomodoro' do
@@ -20,6 +20,24 @@ RSpec.describe Pomodoros::CreateService do
       expect(created_pomodoro.activity_tags).to match_array(
         [programming_activity_tag, daryllxd_activity_tag]
       )
+    end
+  end
+
+  context 'errors' do
+    context 'no tags' do
+      let!(:created_pomodoro) do
+        execute.call(
+          duration: 25,
+          description: 'Did things',
+          tags: []
+        )
+      end
+
+      it 'returns an error specifying that a tag is needed' do
+        expect(Pomodoro.count).to eq 0
+        expect(created_pomodoro).not_to be_valid
+        expect(created_pomodoro.message).to include('no tags')
+      end
     end
   end
 end
