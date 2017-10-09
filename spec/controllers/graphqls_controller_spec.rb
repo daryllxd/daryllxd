2424fa9135a516035e530b5a7ed1a8ef4f00382e
Daryll Santos <daryll.santos@gmail.com>
2017-10-09 19:45:06 +0800

    Fix spec with GraphQlsController.

--- spec/controllers/graphqls_controller_spec.rb
+++ spec/controllers/graphqls_controller_spec.rb
@@ -1,16 +1,16 @@
 # frozen_string_literal: true
 RSpec.describe GraphqlsController, type: :controller do
   it 'initial test for GraphQL works' do
-    create(:pomodoro, description: 'haha', duration: 2)
-    create(:expense, description: 'choho', amount: 2)
+    pomodoro = create(:pomodoro, description: 'haha', duration: 2)
+    expense = create(:expense, description: 'choho', amount: 2)
 
     post :create, params: {
-      query: '
+      query: "
       {
-        pomodoro(id: 1) { description }
-        expense(id: 1) { amount, description }
+        pomodoro(id: #{pomodoro.id}) { description }
+        expense(id: #{expense.id}) { amount, description }
       }
-      '
+      "
     }
 
     expect(json_response['data'].keys).to eq(%w(pomodoro expense))
