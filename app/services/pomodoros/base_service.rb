cb2c3eba87cff6fcff8cca9717cc2b8414d18a51
Daryll Santos <daryll.santos@gmail.com>
2017-12-31 00:20:48 +0800

    Adds Pomodoros::BaseService for shared functionality across Pomodoro services.

---
+++ app/services/pomodoros/base_service.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+module Pomodoros
+  class BaseService
+    def self.call(params)
+      new(params).call
+    end
+
+    def initialize(params); end
+  end
+end
---
+++ spec/services/pomodoros/base_service_spec.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+class PomodoroTestBaseService < Pomodoros::BaseService
+  def call
+    'swag'
+  end
+end
+
+RSpec.describe Pomodoros::BaseService do
+  it 'call' do
+    inherited_class = PomodoroTestBaseService.call('huh')
+
+    expect(inherited_class).to eq('swag')
+  end
+end
