23f59ac753c8fd05839673059b021ee2e7b67b5a
Daryll Santos <daryll.santos@gmail.com>
2018-03-06 12:24:31 +0800

    Only rescue actual JSON::ParserErrors when controller test fails due to JSON parsing.

--- spec/support/helpers/json_response.rb
+++ spec/support/helpers/json_response.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
+
 def json_response
   JSON.parse(response.body)
-rescue
+rescue JSON::ParserError
   raise 'JSON parse error on response.body'
+end
+
+def json_response_data
+  json_response['data']
 end
