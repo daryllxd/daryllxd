64a6b9302087340e6733db0265169243b38f2c0d
Daryll Santos <daryll.santos@gmail.com>
2018-05-14 22:38:30 +0800

    Created action for creating a Pomodoro.

---
+++ app/services/pomodoros/create_pomodoro.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+module Pomodoros
+  class CreatePomodoro
+    extend LightService::Action
+
+    expects :params, :activity_tags
+    promises :pomodoro
+
+    executed do |context|
+      created_pomodoro = Pomodoro.create(create_pomdoro_attributes(context.params[:pomodoro]))
+
+      if created_pomodoro.valid?
+        context.pomodoro = created_pomodoro
+      else
+        context.fail!(error_from(created_pomodoro))
+      end
+    end
+
+    def self.create_pomdoro_attributes(params)
+      {
+        description: params[:description],
+        duration: params[:duration],
+        started_at: Time.current - params[:duration].minutes
+      }
+    end
+
+    def self.error_from(created_pomodoro)
+      DaryllxdError.new(
+        message: 'Unable to create pomodoro', payload: created_pomodoro.errors.full_messages
+      )
+    end
+  end
+end
---
+++ spec/services/pomodoros/create_pomodoro_spec.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+require 'timecop'
+
+RSpec.describe Pomodoros::CreatePomodoro, type: :ls_action do
+  context 'happy path' do
+    it 'creates a Pomodoro with a start time adjusted for the duration' do
+      Timecop.freeze(Time.new(2018, 2, 15, 5, 0, 0)) do
+        create_params = LightService::Context.new(
+          pomodoro: {
+            description: 'Styling and profiling',
+            duration: 90
+          }
+        )
+
+        action_result = described_class.execute(params: create_params, activity_tags: 'irrelevant')
+
+        expect(action_result).to be_success
+
+        created_pomodoro = action_result.pomodoro
+
+        expect(created_pomodoro.description).to eq('Styling and profiling')
+        expect(created_pomodoro.duration).to eq(90)
+        expect(created_pomodoro.started_at).to eq(Time.new(2018, 2, 15, 3, 30, 0))
+      end
+    end
+  end
+
+  context 'failures' do
+    it 'does not create a Pomodoro, and fails the context' do
+      create_params = LightService::Context.new(
+        pomodoro: {
+          description: 'Styling and profiling',
+          duration: -90
+        }
+      )
+
+      action_result = described_class.execute(params: create_params, activity_tags: 'irrelevant')
+
+      expect(action_result).to be_failure
+      expect(action_result.message.payload).to match_array(
+        ['Duration must be greater than or equal to 0']
+      )
+    end
+  end
+end
